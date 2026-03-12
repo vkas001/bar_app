c1493e944c94f3948d31f11139ab687f256067f4
vkas001 <bikash.795424@cct.tu.edu.np>
2026-03-12 13:34:02 +0545

    feat/added responsiveness

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useIsMobile } from "../hooks/useIsMobile";
 
 export default function RootIndex() {
+  const isMobile = useIsMobile();
+
   return (
-    <View className="flex-1 items-center justify-center bg-lime">
-      <Text className="text-xs font-body">Edit app/index.tsx to edit this screen.</Text>
+    <SafeAreaView className="flex-1 bg-yellow">
+    <View className="flex-1 justify-center items-center p-4">
+      <Text className="font-heading mb-4">
+        {isMobile ? "Welcome Mobile User!" : "Welcome Tablet User!"}
+      </Text>
+
+     
+      <TouchableOpacity className="bg-blue p-4 rounded-lg mb-4">
+        <Text className="text-pink text-center font-body">
+          This card resizes automatically on phones.
+        </Text>
+      </TouchableOpacity>
     </View>
+    </SafeAreaView>
   );
 }
